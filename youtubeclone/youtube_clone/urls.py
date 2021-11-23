f5c38fff364c4100ea3ea64ad36814aae5b1756a
danielleleigh <danielle.leigh90@gmail.com>
2021-11-23 15:28:25 -0600

    Added paths for Reply and tested out links

--- youtubeclone/youtube_clone/urls.py
+++ youtubeclone/youtube_clone/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('comment/', views.CommentList.as_view()),
-    path('comment/video_id/', views.CommentDetail.as_view())
+    path('comment/<int:pk>/', views.CommentDetail.as_view()),
+
+    path('reply/', views.ReplyList.as_view()),
+    path('reply/<int:pk>/', views.ReplyDetail.as_view())
 
 
 ]
