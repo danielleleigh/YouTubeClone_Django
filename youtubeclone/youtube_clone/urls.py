98daa489a7486e715bad5affd58060f8abeab25a
danielleleigh <danielle.leigh90@gmail.com>
2021-11-23 11:34:50 -0600

    Test endpoints with postman

--- youtubeclone/youtube_clone/urls.py
+++ youtubeclone/youtube_clone/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('video/', views.VideoList.as_view()),
-    # path('video/<int:pk>/', views.VideoDetail.as_view())
+    path('video/<int:pk>/', views.VideoDetail.as_view())
 ]
